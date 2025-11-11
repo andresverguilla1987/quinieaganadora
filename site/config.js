23a3dffe52bb9ebb94dbc223e1d941ad8f3ee8f7
andresverguilla1987 <92838618+andresverguilla1987@users.noreply.github.com>
2025-11-10 19:14:00 -0600

    Update config.js

--- site/config.js
+++ site/config.js
@@ -1,6 +1,6 @@
 
 // EDITAR AQUÍ: API_BASE debe apuntar a tu backend (sin añadir /api si tu backend expone rutas en la raíz)
 // Ejemplo correcto según tus logs: "https://quinieaganadora.onrender.com"
+// Sin /api
 const API_BASE = "https://quinieaganadora.onrender.com";
-
 export { API_BASE };
